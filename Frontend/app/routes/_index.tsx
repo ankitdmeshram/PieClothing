d31a887a7ff43e9efebfbb975022e332d273313e
Ankit Meshram <ankitdm69@gmail.com>
2023-10-30 13:57:19 +0530

    first commit

---
+++ Frontend/app/routes/_index.tsx
@@ -0,0 +1,45 @@
+import styles from "../styles/index.css";
+const _index = () => {
+  return (
+    <>
+      <div className="gs-product">
+        <div className="gs-container">
+          <div className="gs-row">
+
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+          <div className="gs-col-1">RED</div>
+
+
+
+
+            <div className="gs-col-6">RED</div>
+            <div className="gs-col-6">BLUE</div>
+            <div className="gs-col-6">BLUE</div>
+            <div className="gs-col-6">BLUE</div>
+            <div className="gs-col-6">BLUE</div>
+            <div className="gs-col-6">BLUE</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default _index;
+
+export const links = () => [
+  {
+    rel: "stylesheet",
+    href: styles,
+  },
+];
