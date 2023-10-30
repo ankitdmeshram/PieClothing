3cad73cd37948731dc332c443144ee36941d158f
Ankit Meshram <ankitdm69@gmail.com>
2023-10-30 18:07:52 +0530

    product controller created

---
+++ Frontend/app/controllers/productControllers.tsx
@@ -0,0 +1,41 @@
+import { postAPI } from "~/utils/api";
+
+// all products
+export const productController = () => {
+  try {
+    const body = {};
+    return postAPI("", body);
+  } catch (err) {
+    console.log("Something went wrong", err);
+  }
+};
+
+// add products
+export const addProduct = () => {
+  try {
+    const body = {};
+    return postAPI("", body);
+  } catch (err) {
+    console.log("Something went wrong", err);
+  }
+};
+
+// update products
+export const updateProduct = () => {
+  try {
+    const body = {};
+    return postAPI("", body);
+  } catch (err) {
+    console.log("Something went wrong", err);
+  }
+};
+
+// delete product
+export const deleteProduct = () => {
+  try {
+    const body = {};
+    return postAPI("", body);
+  } catch (err) {
+    console.log("Something went wrong", err);
+  }
+};
